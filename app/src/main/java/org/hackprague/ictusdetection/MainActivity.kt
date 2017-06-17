e7e46c32cad1c75f5c766324185edcf72deb7e60
Eduard Maura <edu.maura.puig@gmail.com>
2017-06-17 22:52:15 +0200

    Add loading functionality and fix design

--- app/src/main/java/org/hackprague/ictusdetection/MainActivity.kt
+++ app/src/main/java/org/hackprague/ictusdetection/MainActivity.kt
@@ -1,17 +1,16 @@
 package org.hackprague.ictusdetection
 
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
-
-import kotlinx.android.synthetic.main.activity_main.*
-import android.content.ContentValues
-import android.os.CountDownTimer
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,23 +27,27 @@ class MainActivity : AppCompatActivity() {
         object : CountDownTimer(TIME_TO_PHOTO, 1000) {
 
             override fun onTick(millisUntilFinished: Long) {
-                countdown.setText("In " + millisUntilFinished / 1000 + " seconds camera will be " +
-                        "open and you should capture a photo of your face ")
+                countdown.text = "In " + millisUntilFinished / 1000 + " seconds camera will be " +
+                        "open and you should capture a photo of your face"
             }
 
             override fun onFinish() {
-                countdown.visibility = View.INVISIBLE
-                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-                val values = ContentValues()
-                values.put(MediaStore.Images.Media.TITLE, "New Picture")
-                values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera")
-                imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
-
-                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+                takePhoto()
             }
         }.start()
+    }
+
+    private fun takePhoto() {
+        countdown.visibility = View.INVISIBLE
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera")
+        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+
+        startActivityForResult(intent, CAMERA_REQUEST_CODE)
     }
 
 
@@ -55,7 +58,10 @@ class MainActivity : AppCompatActivity() {
             CAMERA_REQUEST_CODE -> {
                 if (resultCode == Activity.RESULT_OK && imageUri != null) {
                     photoImageView.visibility = View.VISIBLE
-                    photoImageView.setImageBitmap(MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri))
+                    photoImageView.setImageBitmap(MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri))
+                    progressBar.visibility = View.VISIBLE
+                    progressBar.isIndeterminate = true
+                    Toast.makeText(applicationContext, "Analysing image", Toast.LENGTH_LONG).show()
                 }
             }
         }
