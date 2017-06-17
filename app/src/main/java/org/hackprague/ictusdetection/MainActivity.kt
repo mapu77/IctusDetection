1e897f8ffda6b2fcf16c5bc68fbd7c524d98953a
Sergio <sergio.paredes25@gmail.com>
2017-06-17 21:22:58 +0200

    Remove camera button

--- app/src/main/java/org/hackprague/ictusdetection/MainActivity.kt
+++ app/src/main/java/org/hackprague/ictusdetection/MainActivity.kt
@@ -2,7 +2,6 @@ package org.hackprague.ictusdetection
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -11,11 +10,14 @@ import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.ContentValues
+import android.os.CountDownTimer
+import android.view.View
 
 
 class MainActivity : AppCompatActivity() {
 
     val CAMERA_REQUEST_CODE = 0
+    val TIME_TO_PHOTO: Long = 5000
 
     private var imageUri: Uri? = null
 
@@ -23,17 +25,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        cameraButton.setOnClickListener {
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        object : CountDownTimer(TIME_TO_PHOTO, 1000) {
 
-            val values = ContentValues()
-            values.put(MediaStore.Images.Media.TITLE, "New Picture")
-            values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera")
-            imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            override fun onTick(millisUntilFinished: Long) {
+                countdown.setText("In " + millisUntilFinished / 1000 + " seconds camera will be " +
+                        "open and you should capture a photo of your face ")
+            }
 
-            startActivityForResult(intent, CAMERA_REQUEST_CODE)
-        }
+            override fun onFinish() {
+                countdown.visibility = View.INVISIBLE
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+                val values = ContentValues()
+                values.put(MediaStore.Images.Media.TITLE, "New Picture")
+                values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera")
+                imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }
+        }.start()
     }
 
 
@@ -43,11 +54,9 @@ class MainActivity : AppCompatActivity() {
         when (requestCode) {
             CAMERA_REQUEST_CODE -> {
                 if (resultCode == Activity.RESULT_OK && imageUri != null) {
+                    photoImageView.visibility = View.VISIBLE
                     photoImageView.setImageBitmap(MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri))
                 }
-            }
-            else -> {
-                Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT).show()
             }
         }
     }
