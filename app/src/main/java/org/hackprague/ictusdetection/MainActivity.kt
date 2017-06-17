7c3fe38edc85fef772de266c892ad093c428868a
Sergio <sergio.paredes25@gmail.com>
2017-06-17 20:52:15 +0200

    Add camera capture

--- app/src/main/java/org/hackprague/ictusdetection/MainActivity.kt
+++ app/src/main/java/org/hackprague/ictusdetection/MainActivity.kt
@@ -1,12 +1,54 @@
 package org.hackprague.ictusdetection
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.ContentValues
+
 
 class MainActivity : AppCompatActivity() {
+
+    val CAMERA_REQUEST_CODE = 0
+
+    private var imageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            val values = ContentValues()
+            values.put(MediaStore.Images.Media.TITLE, "New Picture")
+            values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera")
+            imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+
+            startActivityForResult(intent, CAMERA_REQUEST_CODE)
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (resultCode == Activity.RESULT_OK && imageUri != null) {
+                    photoImageView.setImageBitmap(MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri))
+                }
+            }
+            else -> {
+                Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
