0bb9147e870d8cab53267cdf866bc6d4565c9197
Eduard Maura <edu.maura.puig@gmail.com>
2017-06-18 07:26:55 +0200

    Finish second ictus test

--- app/src/main/java/org/hackprague/ictusdetection/ImageTestFragment.kt
+++ app/src/main/java/org/hackprague/ictusdetection/ImageTestFragment.kt
@@ -7,27 +7,49 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_image_test_holder.*
+import java.util.*
 
 /**
  * A placeholder fragment containing a simple view.
  */
 class ImageTestFragment : Fragment() {
 
+    private var labels = listOf("orange", "tree", "window", "house")
+
+    private var indexLabel: Int = 0
+
+    private var generator: Random = Random()
+
+    private var wrong: Int = 0
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        Collections.shuffle(labels, generator)
         return inflater!!.inflate(R.layout.fragment_image_test_holder, container, false)
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        val selectedLabel = labels[indexLabel++]
+        displayImage(selectedLabel)
         next.setOnClickListener({
             val inputText = description.text.toString()
-            val text: String
-            if (inputText.equals("orange")) {
-                text = "Ok"
-            } else {
-                text = "Oops, ictus!"
+            if (selectedLabel != inputText.toLowerCase()) {
+                wrong++
             }
-            Toast.makeText(view!!.context, text, Toast.LENGTH_LONG).show()
+            if (indexLabel < labels.size) {
+                displayImage(labels[indexLabel++])
+            } else {
+                if (wrong < 2) {
+                    //TODO: Jump to next test
+                } else {
+                    Toast.makeText(context, "Oops, ictus!", Toast.LENGTH_LONG).show()
+                }
+            }
         })
+    }
+
+    private fun displayImage(selectedLabel: String) {
+        val identifier = context.resources.getIdentifier(selectedLabel, "drawable", context.packageName)
+        imageTest.setImageDrawable(context.getDrawable(identifier))
     }
 }
